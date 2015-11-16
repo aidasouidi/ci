dd11af1b21facc1aed60a1f3ceeaee7e07aa1f9a
aidasouidi <souidi.aida90@gmail.com>
2015-11-16 19:17:19 +0100

    First commit

---
+++ application/controllers/hello.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* 
+*/
+class Hello extends CI_Controller
+{
+	public function _construct()
+	{ 
+		parent::_construct();
+
+		echo "this is the inialization<br>";
+	}
+
+
+	public function index()
+	{
+		$data = array();
+		$data["name"]="aa";
+		$this->load->view('hello',$data);
+	}
+	
+	public function one($p1,$p2)
+	{
+		//
+		echo "this is the one <br>" ;
+		echo "the are params: $p1, $p2";
+
+	}
+	public function two()
+	{
+		//
+		echo "this is the two";
+	}
+
+	
+}
